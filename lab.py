001f11fd979299af368a5b9ee0b57c414a1301c3
robotkutya <mihalyfuredi@gmail.com>
2016-05-12 09:43:26 +0200

    Added MapRead() and R_Pos

--- lab.py
+++ lab.py
@@ -7,10 +7,9 @@ from curses import wrapper
 
 # Sets up
 def initialize(screen):
-    global y, x, q, dims
+    global q, dims, R_pos
     # Sets global variables
-    y = 0
-    x = 0
+    R_pos = [0,0]
     q = -1
     dims = screen.getmaxyx()
     # Makes the cursor not blink
@@ -18,49 +17,82 @@ def initialize(screen):
 
 # Draws Rezso on the screen
 def drawRezso(screen):
-    screen.addstr(y, x, 'R', curses.A_BOLD)
+    screen.addstr(R_pos[0], R_pos[1], 'R', curses.A_BOLD)
+
+# Reads and interprets the map file into memory
+def readMap(screen):
+    global wall_coordinates, R_pos
+    wall_coordinates = set()
+    wall_char = {'#'}
+    start_char = {'S','s'}
+    a = open('map', 'r')
+    f = a.readlines()
+    for j in range(0,len(f)):
+        for i in range(0,len(f[0])):
+            if f[j][i] in start_char:
+                R_pos = [j,i]
+            if f[j][i] in wall_char:
+                #this should follow the curses coordinate system
+                wall_coordinates.add((j,i))
+            else:
+                pass
+
+## need to make coordinates like in curses
 
 # Draws the map (walls, exit, etc) on the screen
 def drawMap(screen):
-    pass
+    a = open('map', 'r')
+    f = a.read()
+    screen.addstr(0, 0, f)
 
 # Controls the movement of Rezso, the 'R' character on screen
 def movement(screen):
     # Get global variables
-    global y, x, dims, q
+    global R_pos, R_pos_previous, dims, q
+    # Save Rezso's position
+    R_pos_previous = R_pos
     # Get user input
     q = screen.getch()
     # Movement itself
-    # Gets max y and x coordinates in a list
-    if q == curses.KEY_UP and y > 0:
-        y -= 1
-    elif q == curses.KEY_DOWN and y < dims[0]-2:
-        y += 1
-    elif q == curses.KEY_LEFT and x > 0:
-        x -= 1
-    elif q == curses.KEY_RIGHT and x < dims[1]-2:
-        x += 1
+    if q == curses.KEY_UP and R_pos[0] > 0:
+        R_pos[0] -= 1
+    elif q == curses.KEY_DOWN and R_pos[0] < dims[0]-1:
+        R_pos[0] += 1
+    elif q == curses.KEY_LEFT and R_pos[1] > 0:
+        R_pos[1] -= 1
+    elif q == curses.KEY_RIGHT and R_pos[1] < dims[1]-1:
+        R_pos[1] += 1
+    else:
+        pass
 
 # Decides what happens when Rezso hits a wall or an exit
 def checker(screen):
-    pass
+    global R_pos, R_pos_previous
+    if (R_pos[0], R_pos[1]) in wall_coordinates:
+        screen.addstr(16, 0, 'Wall')
+        R_pos = [1,1]
+    else:
+        pass
 
 # Draws menu, gives tutorial, explains the game
 def menu(screen):
     pass
 
 # Define the main() function for the wrapper
-
 def main(screen):
-    # Get global variables
-    global y, x, dims, q
+    # We need to pass global variables to the wrapper
+    global q
     initialize(screen)
+    readMap(screen)
     menu(screen)
     # Make the program run continuously and exit when 'q' is pressed
     while q != ord('q'):
         screen.clear()
-        drawRezso(screen)
         drawMap(screen)
+        drawRezso(screen)
+        checker(screen)
+        screen.addstr(15,0,str(R_pos[0]))
+        screen.addstr(15,5,str(R_pos[1]))
         movement(screen)
         checker(screen)
         screen.refresh()
